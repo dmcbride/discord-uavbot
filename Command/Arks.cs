65790e343f1e8970621c71ac327b58c906743828
Darin McBride <dmcbride@cpan.org>
2021-08-12 20:30:37 -0600

    Add letter & sci notation for cw and ark output.
    Fixes #2

--- Command/Arks.cs
+++ Command/Arks.cs
@@ -54,7 +54,7 @@ namespace uav.Command
             var dm = Math.Floor(arks * 40 / 60);
 
             return ReplyAsync(
-                $@"To get to a GV of {goalGv} from {gv} starting with cash-on-hand of {cash}, you need {arks} <:boostcashwindfall:642974216681029644> arks.
+                $@"To get to a GV of {DualString(goalGvValue)} from {DualString(gvValue)} starting with cash-on-hand of {DualString(cashValue)}, you need {arks} <:boostcashwindfall:642974216681029644> arks.
 At about 6 <:boostcashwindfall:642974216681029644> arks per hour, that is about {hours} hour{(hours == 1 ? string.Empty:"s")}.
 During this time, you can expect to get about {dm} <:ipmdm:628302645265956875> arks, for a total of {5 * dm} <:ipmdm:628302645265956875>.");
         }
@@ -76,10 +76,11 @@ During this time, you can expect to get about {dm} <:ipmdm:628302645265956875> a
             }
 
             var cws = ArkCalculate(gvValue, goalGvValue, gvValue, 1.1);
-            return ReplyAsync($"To get to a GV of {goalGv} from {gv}, you need {cws} cash windfalls.");
+            return ReplyAsync($"To get to a GV of {DualString(goalGvValue)} from {DualString(gvValue)}, you need {cws} cash windfalls.");
         }
 
         private static readonly IReadOnlyDictionary<string, int> suffixExponent;
+        private static readonly IReadOnlyDictionary<int, string> toSuffixExponent;
 
         static Arks()
         {
@@ -88,6 +89,7 @@ During this time, you can expect to get about {dm} <:ipmdm:628302645265956875> a
                 }.Concat(Enumerable.Range((int)'a', (int)'m' - (int)'a' + 1).Select(i => "a" + (char)i))
                 .Select((s, i) => (s, i: 3*(i+1)))
                 .ToDictionary(k => k.s, k => k.i);
+            toSuffixExponent = s.ToDictionary(kv => kv.Value, kv => kv.Key); // reverse lookup
             s["m"] = s["M"];
             s["K"] = s["k"];
             suffixExponent = s;
@@ -121,6 +123,29 @@ During this time, you can expect to get about {dm} <:ipmdm:628302645265956875> a
                 return true;
             }
             return false;
+        }
+
+        private (string letter, string exp) ToStrings(double qty)
+        {
+            var powerOf10 = (int) Math.Floor(Math.Log10(qty));
+            var powerOf1000 = powerOf10 / 3;
+
+            string letter = null;
+            if (toSuffixExponent.TryGetValue(powerOf1000 * 3, out var letterSuffix))
+            {
+                letter = $"{qty / Math.Pow(1000d, powerOf1000):g5}{letterSuffix}";
+            }
+
+            var exp = $"{qty / Math.Pow(10d, powerOf10):g2}E+{powerOf10}";
+
+            return (letter, exp);
+        }
+
+        private string DualString(double qty)
+        {
+            var (letter, exp) = ToStrings(qty);
+
+            return letter == null ? exp : $"{letter} ({exp})";
         }
     }
 }
