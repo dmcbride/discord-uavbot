2880fc470aa0e33fe8903cef51473c88958619db
Darin McBride <dmcbride@cpan.org>
2023-07-26 20:41:14 -0600

    Make the base credit extractor a bit smarter.
    Use the known range to see if the extraction got something reasonable.

--- uav.logic/Service/ExtractGvBaseCredits.cs
+++ uav.logic/Service/ExtractGvBaseCredits.cs
@@ -8,7 +8,7 @@ public class ExtractGvBaseCredits : TesseractExtractor<ExtractGvBaseCredits.Extr
   public record ExtractedValues(GV gv, int credits);
 
   private Regex gvExtractor = new Regex(@"Galaxy Value\s*\$(\d+\.?\d*[a-zA-Z]{1,2}|\d+\.?\d*E\+\d+)");
-  private Regex creditsExtractor = new Regex(@"Base Reward\s*(\d+)(?!=\s*\d)");
+  private Regex creditsExtractor = new Regex(@"Base Reward\s*(\d+)(?:\s*(\d+))");
   protected override ExtractedValues? FindValues(string text)
   {
     if (string.IsNullOrWhiteSpace(text))
@@ -22,7 +22,21 @@ public class ExtractGvBaseCredits : TesseractExtractor<ExtractGvBaseCredits.Extr
         GV.TryFromString(gvMatch.Groups[1].Value, out var gv, out _) &&
         int.TryParse(creditMatch.Groups[1].Value, out var credits))
     {
-      return new ExtractedValues(gv, credits);
+      var creditService = new Credits();
+      var expectedMinimumCredits = creditService.TierCredits(gv);
+      var expectedMaximumCredits = creditService.TierCredits(gv, 1);
+
+      if (credits >= expectedMinimumCredits && credits < expectedMaximumCredits)
+      {
+        return new ExtractedValues(gv, credits);
+      }
+
+      if (creditMatch.Groups.Count > 2 && creditMatch.Groups[2].Success && creditMatch.Groups[2].Length > 0 &&
+          int.TryParse(string.Join("", creditMatch.Groups[1].Value, creditMatch.Groups[2].Value), out var credits2) &&
+          credits2 >= expectedMinimumCredits && credits2 < expectedMaximumCredits)
+      {
+        return new ExtractedValues(gv, credits2);
+      }
     }
     return null;
   }
